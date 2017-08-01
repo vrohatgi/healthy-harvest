e0f08456416a410ae7491f0e1200ee77c63621e1
Vanya Rohatgi <vanyarohatgi@yahoo.com>
2017-07-31 21:36:45 -0700

    got logout working for events tab, need it on all tabs

---
+++ TripPlanner/Controllers/EventsViewController.swift
@@ -0,0 +1,55 @@
+////
+////  EventViewController.swift
+////  TripPlanner
+////
+////  Created by vanya rohatgi on 7/24/17.
+////  Copyright © 2017 Vanya Rohatgi. All rights reserved.
+////
+//
+import Foundation
+import UIKit
+import FirebaseAuth
+
+class EventsViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    var authHandle: AuthStateDidChangeListenerHandle?
+    
+    // MARK: - VC Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        authHandle = Auth.auth().addStateDidChangeListener() { [unowned self] (auth, user) in
+            guard user == nil else { return }
+            
+            let loginViewController = UIStoryboard.initialViewController(for: .login)
+            self.view.window?.rootViewController = loginViewController
+            self.view.window?.makeKeyAndVisible()
+        }
+    }
+    
+    deinit {
+        if let authHandle = authHandle {
+            Auth.auth().removeStateDidChangeListener(authHandle)
+        }
+    }
+    
+    // MARK: - IBOutlets
+    
+    
+    // MARK: - IBActions
+    
+    @IBAction func didTapLogOutButton(_ sender: UIBarButtonItem) {
+        print("i am in logout!")
+        
+        do {
+            try Auth.auth().signOut()
+        } catch let error as NSError {
+            assertionFailure("Error signing out: \(error.localizedDescription)")
+        }
+        
+        print("iamsignedout")
+    }
+}
