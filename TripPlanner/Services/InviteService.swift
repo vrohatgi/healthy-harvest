9312dca40ffea777b40b1bfe57a85e36b525c0dc
Vanya Rohatgi <vanyarohatgi@yahoo.com>
2017-08-04 14:39:17 -0700

    removed useless code

--- TripPlanner/Services/InviteService.swift
+++ TripPlanner/Services/InviteService.swift
@@ -11,46 +11,7 @@ import UIKit
 import FirebaseDatabase
 
 struct InviteService {
-    
-    private static func inviteUser(_ user: User, places: [Place], eventName: String, forCurrentUserWithSuccess success: @escaping (Bool) -> Void) {
         
-        let currentUID = User.current.uid
-        let inviteData = ["peopleWhoInvitedMe/\(user.uid)/\(currentUID)" : true, "friendsInvited/\(currentUID)/\(user.uid)" : true]
-        
-        let ref = Database.database().reference()
-        ref.updateChildValues(inviteData) { (error, _) in
-            if let error = error {
-                assertionFailure(error.localizedDescription)
-                success(false)
-            }
-        }
-    }
-    
-    private static func uninviteUser(_ user: User, forCurrentUserWithSuccess success: @escaping (Bool) -> Void) {
-        let currentUID = User.current.uid
-        let inviteData = ["peopleWhoInvitedMe/\(user.uid)/\(currentUID)" : NSNull(),
-                          "friendsInvited/\(currentUID)/\(user.uid)" : NSNull()]
-        
-        let ref = Database.database().reference()
-        ref.updateChildValues(inviteData) { (error, ref) in
-            if let error = error {
-                assertionFailure(error.localizedDescription)
-            }
-            
-            success(error == nil)
-        }
-    }
-    
-    // follower = friend who got invited; followee = friend who invited current user
-    
-    static func setIsInvited(_ isFollowing: Bool, places: [Place], eventName: String, fromCurrentUserTo followee: User, success: @escaping (Bool) -> Void) {
-        if isFollowing {
-            inviteUser(followee, places: places, eventName: eventName, forCurrentUserWithSuccess: success)
-        } else {
-            uninviteUser(followee, forCurrentUserWithSuccess: success)
-        }
-    }
-    
     static func isUserInvited(_ user: User,
                               byCurrentUserWithCompletion completion: @escaping (Bool) -> Void) {
         
