83863f70e0ec65e67f566a16e18badd6c78651d8
Sumeet Rohatgi <sum.rohatgi@gmail.com>
2017-08-05 01:54:19 -0700

    added vote button
    still not working

--- TripPlanner/Views/VotingTableViewCell.swift
+++ TripPlanner/Views/VotingTableViewCell.swift
@@ -9,8 +9,14 @@
 import Foundation
 import UIKit
 
+protocol VotingTableViewCellDelegate: class {
+    func didTapVoteButton(_ voteButton: UIButton, on cell: VotingTableViewCell)
+}
+
 class VotingTableViewCell: UITableViewCell {
     
+    weak var delegate: VotingTableViewCellDelegate?
+
     // MARK: -IBOutlets
     
     @IBOutlet weak var totalVotesLabel: UILabel!
@@ -21,8 +27,20 @@ class VotingTableViewCell: UITableViewCell {
     
     // MARK: -IBActions
     
-    @IBAction func didTapVoteButton(_ sender: Any) {
+    @IBAction func didTapVoteButton(_ sender: UIButton) {
+        delegate?.didTapVoteButton(sender, on: self)
     }
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        //        inviteButton.layer.borderColor = UIColor.lightGray.cgColor
+        //        inviteButton.layer.borderWidth = 1
+        //        inviteButton.layer.cornerRadius = 6
+        //        inviteButton.clipsToBounds = true
+        
+        voteButton.setTitle("Vote", for: .normal)
+        voteButton.setTitle("Voted", for: .selected)
+    }
     
 }
