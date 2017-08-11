e527d9a210a21c41358668e5304344b6217a1a5b
Sumeet Rohatgi <sum.rohatgi@gmail.com>
2017-08-10 21:32:53 -0700

    fixed report user bug

--- TripPlanner/Services/UserService.swift
+++ TripPlanner/Services/UserService.swift
@@ -14,14 +14,8 @@ struct UserService {
     
     static func report(uid: String) {
         let ref = Database.database().reference().child("users").child(uid)
-
-        ref.observeSingleEvent(of: .value, with: { (snapshot) in
-            guard let user = User(snapshot: snapshot) else {
-                return
-            }
-            
-            ref.setValue(["username": user.username, "isReported": true])
-        })
+        
+        ref.updateChildValues(["isReported": true])
     }
     
     static func show(forUID uid: String, completion: @escaping (User?) -> Void) {
@@ -46,7 +40,7 @@ struct UserService {
                 return completion(nil, "Username: \(username) already exists")
             }
             
-            let userAttrs = ["username": username]
+            let userAttrs = ["username": username, "isReported": false] as [String : Any]
             
             let ref = Database.database().reference().child("users").child(firUser.uid)
             ref.setValue(userAttrs) { (error, ref) in
